15f2e47cf17ae1e141ae6ed709966e2675e0e704
Dylan McIntyre <dwillmc@gmail.com>
2017-04-12 12:02:28 -0400

    update

--- libwebp/src/main/jni/Application.mk
+++ libwebp/src/main/jni/Application.mk
@@ -1,3 +1,3 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := arm64-v8a armeabi armeabi-v7a mips mips64 x86 x86_64
 APP_PLATFORM := android-9
